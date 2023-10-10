e3c2b430ef5351b0d2fc2f2293e4468b0281a667
Aliaksandr Hryharovich <algrigr@gmail.com>
2023-10-10 12:52:22 +0300

    Initial commit

---
+++ src/main/java/com/flashsan/cassandraWithSpringBoot/CassandraWithSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.flashsan.cassandraWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+
+public class CassandraWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashsan/cassandraWithSpringBoot/CassandraWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashsan.cassandraWithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
